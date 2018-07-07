2adc409bd7bb58499218f4f487c825e55306e46d
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-08 01:40:17 +0900

    Add configuration for ignoring schemes
    This adds a new `g:xcode_scheme_ignore_pattern` global variable that, if set,
    will determine which schemes to hide when listing/finding schemes. This is
    useful for cases where you have demo/example schemes that you don't want
    included in the list of available schemes. If this isn't set, the old behavior
    is maintained

--- bin/list_schemes.sh
+++ bin/list_schemes.sh
@@ -2,7 +2,31 @@
 
 set -o pipefail
 
-xcrun xcodebuild -list "$@" 2>/dev/null \
+while getopts "f:t:i:" opt; do
+  case $opt in
+    f) target_type_flag="$OPTARG";;
+    t) target="$OPTARG";;
+    i) ignore_pattern="$OPTARG";;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+  esac
+done
+
+schemes="$(
+  xcrun xcodebuild -list "$target_type_flag" "$target" 2>/dev/null \
   | awk '/Schemes:/,0' \
   | tail -n +2 \
   | sed -e "s/^[[:space:]]*//"
+)"
+
+if [ -z "$ignore_pattern" ]; then
+  echo "$schemes"
+else
+  echo "$schemes" | sed -E "$ignore_pattern"
+fi
