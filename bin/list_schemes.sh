b56a81ad97c1bf11be80df495dcfe95f1ca812c9
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-12 23:22:56 +0900

    Make list_schemes read args to see if ignore pattern present

--- bin/list_schemes.sh
+++ bin/list_schemes.sh
@@ -2,17 +2,29 @@
 
 set -o pipefail
 
-ignore_pattern="$1"
+while getopts "t:i:" opt; do
+    case $opt in
+        t) target="$OPTARG";;
+        i) ignore_pattern="$OPTARG";;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+    esac
+done
 
-if [[ "$ignore_pattern" == "-workspace" ]]; then
+schemes="$(
     xcrun xcodebuild -list 2>/dev/null \
-      | awk '/Schemes:/,0' \
-      | tail -n +2 \
-      | sed -e "s/^[[:space:]]*//"
-else
-    xcrun xcodebuild -list 2>/dev/null \
-      | awk '/Schemes:/,0' \
-      | tail -n +2 \
-      | sed -E "$ignore_pattern" \
-      | sed -e "s/^[[:space:]]*//"
+    | awk '/Schemes:/,0' \
+    | tail -n +2 \
+    | sed -e "s/^[[:space:]]*//"
+)"
+
+if [[ $ignore_pattern ]]; then
+    sed -E "$ignore_pattern"
 fi
+
