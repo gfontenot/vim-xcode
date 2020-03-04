f71a777412f97552fb7601cf4355f3d824ba8b72
Jan Dammshäuser <mail@jandamm.de>
2020-03-04 18:29:30 +0100

    Remove schemes from excluded sources

--- bin/list_schemes.sh
+++ bin/list_schemes.sh
@@ -2,14 +2,30 @@
 
 set -o pipefail
 
-ignore_schemes=()
+get_schemes() {
+  xcrun xcodebuild -list "$1" "$2" 2>/dev/null \
+    | awk '/Schemes:/,0' \
+    | tail -n +2 \
+    | sed -e "s/^[[:space:]]*//"
+}
+
+get_sources_ignore() {
+  case "$1" in
+    *xcworkspace) type='-workspace';;
+    *xcodeproj) type='-project';;
+    *) return;;
+  esac
+  echo "^($(get_schemes "$type" "$1" | tr '\n' '|' | sed 's/|*$//'))$"
+}
+
+ignore_sources_pattern=""
 
 while getopts "f:t:i:e:" opt; do
   case $opt in
     f) target_type_flag="$OPTARG";;
     t) target="$OPTARG";;
     i) ignore_pattern="$OPTARG";;
-    e) ignore_schemes+=("$OPTARG");;
+    e) ignore_sources_pattern="$ignore_sources_pattern|$(get_sources_ignore "$OPTARG")";;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       exit 1
@@ -21,12 +37,12 @@ while getopts "f:t:i:e:" opt; do
   esac
 done
 
-schemes="$(
-  xcrun xcodebuild -list "$target_type_flag" "$target" 2>/dev/null \
-  | awk '/Schemes:/,0' \
-  | tail -n +2 \
-  | sed -e "s/^[[:space:]]*//"
-)"
+schemes="$(get_schemes "$target_type_flag" "$target")"
+
+if [ -n "$ignore_sources_pattern" ]; then
+  ignore_sources_pattern="/$(echo "$ignore_sources_pattern" | sed 's/^|*//')/d"
+  schemes="$(echo "$schemes" | sed -E "$ignore_sources_pattern")"
+fi
 
 if [ -z "$ignore_pattern" ]; then
   echo "$schemes"
